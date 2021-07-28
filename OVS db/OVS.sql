18f4442c824b4283ffa55fa903f6dbba664c8d19
MatomeMab <62009843+MatomeMab@users.noreply.github.com>
2021-07-28 11:10:01 +0200

    Update OVS.sql

--- OVS db/OVS.sql
+++ OVS db/OVS.sql
@@ -189,6 +189,97 @@ Order_Status_Description varchar(50)
 
  )
 
+Create table Quote_Status
+(
+Quote_Status_ID int primary key ,
+Quote_Status_Description varchar(50)
+)
+
+Create table Request_Quote
+(
+Request_Quote_ID int primary key ,
+Date Datetime,
+Quote_Status int references Quote_Status(Quote_Status_ID)
+)
+
+Create table Quote_Line
+(
+Quote_Line_ID int primary key ,
+Quantity varchar(50),
+Product int references Product(Product_ID),
+Request_Quote_ID int references Request_Quote(Request_Quote_ID)
+)
+
+Create table Sale
+(
+Sale_ID int primary key ,
+Sale_Date Datetime,
+Request_Quote int references Request_Quote(Request_Quote_ID)
+)
+
+Create table Sale_Line
+(
+Sale_Line_ID int primary key,
+Sale int references Sale(Sale_ID),
+Product int references Product(Product_ID)
+)
+
+Create table Return_Sale_Request
+(
+Return_Sale_Request_ID int primary key,
+Return_Request_Date Datetime,
+Customer int references Customer(Customer_ID)
+)
+
+Create table Return_Sale_Request_SaleLine
+(
+Quantity varchar(5),
+Return_Sale_Reason varchar(100),
+Return_Sale_Request int references Return_Sale_Request(Return_Sale_Request_ID),
+Product int references Product(Product_ID),
+Customer int references Customer(Customer_ID),
+Sale int references Sale(Sale_ID)
+)
+
+Create table Sale_Payment_Status
+(
+Sale_Payment_Status_ID int primary key,
+Sale_Payment_Status_Desc varchar(50)
+)
+
+Create table Payment_Type
+(
+Payment_Type_ID int primary key,
+Payment_Type_Description varchar(50),
+Payment_Type_Name varchar(20)
+)
+
+Create table Sale_Payment
+(
+Sale_Payment_ID int primary key,
+Sale_Payment_Amount varchar(10),
+Sale_Payment_Date Datetime,
+Sale_Payment_Name varchar(50),
+Sale_Payment_Status int references Sale_Payment_Status(Sale_Payment_Status_ID),
+Sale_ID int references Sale(Sale_ID),
+Payment_Type_ID int references Payment_Type(Payment_Type_ID))
+
+Create table Order_Payment_Status
+(
+Order_Payment_Status_ID int primary key,
+Order_Payment_Status_Description varchar(100)
+)
+
+Create table Order_Payment
+(
+Order_Payment_ID int primary key,
+Order_Payment_Amount varchar(10),
+Order_Payment_Date Datetime,
+Customer int references Customer(Customer_ID),
+Payment_Type_ID int references Payment_Type(Payment_Type_ID),
+Order_Payment_Status int references Order_Payment_Status(Order_Payment_Status_ID),
+Order_ID int references [Order](Order_ID)
+)
 
  
 
