a1b8677b7b002c46acf324b9090823538173de11
pride1014 <83179646+pride1014@users.noreply.github.com>
2021-08-03 16:15:22 +0200

    Update OVS.sql
    Co-Authored-By: ChaseNel <62016103+ChaseNel@users.noreply.github.com>
    Co-Authored-By: MatomeMab <62009843+MatomeMab@users.noreply.github.com>

--- OVS db/OVS.sql
+++ OVS db/OVS.sql
@@ -46,7 +46,7 @@ Shift_Type_ID int identity(1,1) primary key not null,
 Shift_Type_Description varchar(100) not null 
 )
 
-CREATE TABLE Shift (
+CREATE TABLE [Shift] (
 Shift_ID int identity(1,1) primary key not null,
 Shift_Type_ID int foreign key references Shift_Type(Shift_Type_ID)
 )
@@ -114,8 +114,9 @@ Size_ID int foreign key references Size(Size_ID)
 )
 
 Create table Product_Special (
-Product_Size_ID int references Product_Size(Product_Size_ID),
-Special_ID int references Special(Special_ID),
+Product_Special_ID int identity(1,1) primary key not null,
+Product_Size_ID int foreign key references Product_Size(Product_Size_ID),
+Special_ID int foreign key references Special(Special_ID),
 Price_Amount float (10) not null
 )
 
@@ -127,9 +128,10 @@ Product_Size_ID int foreign key references Product_Size(Product_Size_ID),
 
 
 Create table Date_Time_Slot (
-Shift_ID int references Shift(Shift_ID),
-Time_Slot_ID int references Time_Slot(Time_Slot_ID),
-Date_ID int references [Date] (Date_ID)
+Date_Time_Slot_ID int identity(1,1) primary key not null,
+Shift_ID int foreign key references Shift(Shift_ID),
+Time_Slot_ID int foreign key references Time_Slot(Time_Slot_ID),
+Date_ID int foreign key references [Date] (Date_ID)
 )
 
 CREATE TABLE Cash_Register  (
@@ -139,14 +141,16 @@ Branch_ID int foreign key references Branch(Branch_ID)
 )
 
 CREATE TABLE Shift_Branch ( 
-Shift_ID int references Shift(Shift_ID),
-Branch_ID int references Branch(Branch_ID)
+Shift_Branch_ID int identity(1,1) primary key not null,
+Shift_ID int foreign key references Shift(Shift_ID),
+Branch_ID int foreign key references Branch(Branch_ID)
 )
 
 CREATE TABLE Shift_Branch_Employee ( 
-Shift_ID int references Shift(Shift_ID),
-Branch_ID int references Branch(Branch_ID),
-Employee_ID int references Employee(Employee_ID)
+Shift_Branch_Employee_ID int identity(1,1) primary key not null,
+Shift_Branch_ID int foreign key references Shift_Branch(Shift_Branch_ID),
+
+Employee_ID int foreign key references Employee(Employee_ID)
 )
 
 
@@ -186,8 +190,9 @@ CREATE TABLE [dbo].[Customer](
 
   Create table Cart_Line 
  (
- Cart_ID int references Cart (Cart_ID),
- Product_ID int references Product (Product_ID),
+ Cart_Line_ID int identity(1,1) primary key not null,
+ Cart_ID int foreign key references Cart (Cart_ID),
+ Product_ID int foreign key references Product (Product_ID),
  Quantity int NULL
  )
 
@@ -197,8 +202,9 @@ CREATE TABLE [dbo].[Customer](
  )
 
  Create table Wishlist_Product (
- Product_ID int references Product(Product_ID),
- Wishlist_ID int references Wishlist(Wishlist_ID),
+ Wishlist_Product_ID int identity(1,1) primary key not null,
+ Product_ID int foreign key references Product(Product_ID),
+ Wishlist_ID int foreign key references Wishlist(Wishlist_ID),
  Quantity int not null
  )
 
@@ -221,11 +227,19 @@ Order_Status_Description varchar(50)
 
  Create table Order_Line 
  (
- Order_ID int references [Order](Order_ID),
- Product_ID int references Product (Product_ID),
+ Order_Line_ID int primary key identity(1,1),
+ Order_ID int ,
+ Product_ID int ,
   Quantity int null,
- )
 
+  CONSTRAINT FK_Order_ID
+	FOREIGN KEY (Order_ID)
+	REFERENCES [Order](Order_ID),
+	
+	CONSTRAINT FK_Product_ID
+	FOREIGN KEY (Product_ID)
+	REFERENCES Product(Product_ID)
+)
  
  Create table Return_Order_Request
  (
@@ -237,11 +251,21 @@ Order_Status_Description varchar(50)
 
  Create table Return_Order_Request_Order_Line
  (
-  Customer_ID int references Customer(Customer_ID),
- Order_ID int references [Order](Order_ID),
- Product_ID int references Product (Product_ID),
+ Return_Order_Request_Order_Line_ID int primary key identity(1,1),
+ 
+ Return_Order_Request_ID int,
+ Order_Line_ID int ,
  Return_Reason varchar(100) not null ,
  Quantity int not null,
+
+	CONSTRAINT FK_Order_Line_ID
+	FOREIGN KEY (Order_Line_ID)
+	REFERENCES Order_Line(Order_Line_ID),
+
+	CONSTRAINT FK_Return_Order_Request_ID
+	FOREIGN KEY (Return_Order_Request_ID)
+	REFERENCES Return_Order_Request(Return_Order_Request_ID)
+
  )
 
  Create Table Credit_Note
@@ -267,9 +291,10 @@ Quote_Status_ID int foreign key references Quote_Status(Quote_Status_ID)
 
 Create table Quote_Line
 (
+Quote_Line_ID int primary key identity(1,1) not null,
 Quantity int not null,
-Product_ID int references Product(Product_ID),
-Request_Quote_ID int references Request_Quote(Request_Quote_ID)
+Product_ID int foreign key references Product(Product_ID),
+Request_Quote_ID int foreign key references Request_Quote(Request_Quote_ID)
 )
 
 Create table Sale
@@ -281,9 +306,18 @@ Request_Quote_ID int foreign key references Request_Quote(Request_Quote_ID)
 
 Create table Sale_Line
 (
-Sale_ID int references Sale(Sale_ID),
-Product_ID int references Product(Product_ID),
+Sale_Line_ID int primary key identity(1,1) not null,
+Sale_ID int ,
+Product_ID int foreign key references Product(Product_ID),
 Quantity int not null,
+
+	CONSTRAINT FK_Sale_ID
+	FOREIGN KEY (Sale_ID)
+	REFERENCES Sale(Sale_ID),
+	
+	--CONSTRAINT FK_Product_ID
+	--FOREIGN KEY (Product_ID)
+	--REFERENCES Product(Product_ID)
 )
 
 Create table Return_Sale_Request
@@ -295,11 +329,20 @@ Customer_ID int foreign key references Customer(Customer_ID)
 
 Create table Return_Sale_Request_Sale_Line
 (
-Quantity int primary key identity(1,1) not null,
+Return_Sale_Request_Sale_Line_ID int primary key identity(1,1) not null,
 Return_Sale_Reason varchar(100),
-Return_Sale_Request_ID int references Return_Sale_Request(Return_Sale_Request_ID),
-Product_ID int references Product(Product_ID),
-Sale_ID int references Sale(Sale_ID)
+Return_Sale_Request_ID int,
+Sale_Line_ID int ,
+
+Quantity int null,
+
+	CONSTRAINT FK_Sale_Line_ID
+	FOREIGN KEY (Sale_Line_ID)
+	REFERENCES Sale_Line(Sale_Line_ID),
+
+	CONSTRAINT FK_Return_Sale_Request_ID
+	FOREIGN KEY (Return_Sale_Request_ID)
+	REFERENCES Return_Sale_Request(Return_Sale_Request_ID)
 )
 
 Create table Sale_Payment_Status
@@ -369,6 +412,7 @@ Order_ID int foreign key references [Order](Order_ID)
  )
  
    create table Supplier_Order_Line (
+   Supplier_Order_Line_ID int identity(1,1) primary key not null,
  Supplier_Order_ID int references Supplier_Order(Supplier_Order_ID),
  Quantity int not null,
  Product_ID int  references Product(Product_ID),
@@ -388,6 +432,7 @@ Order_ID int foreign key references [Order](Order_ID)
  )
 
  create table Reciple_Line(
+ Reciple_Line_ID int Identity(1,1) primary key not null,
  Quantity int not null,
  Recipe_ID int references Recipe(Recipe_ID),
  Raw_Material_ID int foreign key references Raw_Material(Raw_Material_ID),
@@ -401,6 +446,7 @@ Order_ID int foreign key references [Order](Order_ID)
 
   Create table Job_Task_Status(
  Job_Task_Status_ID int identity(1,1) primary key not null,
+ Job_Task_Description_Status varchar (100) not null
  )
 
   Create table Task(
@@ -421,25 +467,27 @@ Order_ID int foreign key references [Order](Order_ID)
  )
 
  Create table Job_task(
- Job_ID int references Job(Job_ID),
- Task_ID int references Task(Task_ID),
+ Job_task_ID int identity(1,1) primary key not null,
+ Job_ID int foreign key references Job(Job_ID),
+ Task_ID int foreign key references Task(Task_ID),
  Job_Task_Status_ID int foreign key references Job_Task_Status(Job_Task_Status_ID),
  Job_Task_Type_ID int foreign key references Job_Task_Type(Job_Task_Type_ID),
  )
 
  Create Table Job_Instance(
- Job_ID int foreign key references Job(Job_ID),
- Employee_ID int foreign key references Employee(Employee_ID),
- Shift_ID int foreign key references Shift(Shift_ID),
-Branch_ID int foreign key references Branch(Branch_ID),
+ Job_Instance_ID int identity(1,1) primary key not null,
+Job_task_ID int foreign key references Job_task(Job_task_ID),
+ Shift_Branch_Employee_ID int foreign key references Shift_Branch_Employee(Shift_Branch_Employee_ID),
+
  [Start_Date] date not null,
-[End_Date] date not null,
+ [End_Date] date not null,
  )
 
  Create Table Job_Instance_Task(
- Job_ID int foreign key references Job(Job_ID),
- Task_ID int foreign key references Task(Task_ID),
+ Job_Instance_Task_ID int identity(1,1) primary key not null,
+ Job_task_ID int foreign key references Job_task(Job_task_ID),
+ Job_Instance_ID int foreign key references Job_Instance(Job_Instance_ID),
  [Start_Date] date not null,
-[End_Date] date not null,
+ [End_Date] date not null,
  )
 
