46d8099cf7c728fa6a63bb81efe18f0e5e77c164
bonganvin <bonganvin@gmail.com>
2021-08-08 17:41:31 +0200

    Bongani's changes

---
+++ OVS Team 3 API/Controllers/Manager Subsystem/BranchController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OVS_Team_3_API.Controllers.Manager_Subsystem
+{
+    public class BranchController : ApiController
+    {
+    }
+}
---
+++ OVS Team 3 API/ViewModels/Employee Subsystem/EmployeeVM.cs
@@ -0,0 +1,22 @@
+﻿using OVS_Team_3_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OVS_Team_3_API.ViewModels.Employee_Subsystem
+{
+    public class EmployeeVM
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public string EmployeePhoneNumber { get; set; }
+        public string EmployeeEmailAddress { get; set; }
+        public Nullable<int> EmployeeTypeID { get; set; }
+        public Nullable<int> UserID { get; set; }
+
+        public virtual Employee_Type EmployeeType { get; set; }
+        public virtual User User { get; set; }
+    }
+}
