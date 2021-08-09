e75d42d452788976b6c984edc81870add0482cf1
ChaseNel <62016103+ChaseNel@users.noreply.github.com>
2021-08-09 11:56:31 +0200

    Updated API Name

---
+++ OVS  API/Controllers/Order/ReturnOrderRequestController.cs
@@ -0,0 +1,56 @@
+﻿using OVS_Team_3_API.Models;
+using OVS_Team_3_API.ViewModels.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OVS_Team_3_API.Controllers.Order
+{
+    [RoutePrefix("api/returnorder")]
+    public class ReturnOrderRequestController : ApiController
+    {
+
+        OVSEntities5 db = new OVSEntities5();
+        // GET: ReturnOrder
+        [Route("Getreturnorder")]
+        [HttpGet]
+        public List<ReturnOrderRequestVM> GetUserAccess()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Return_Order_Request.Select(zz => new ReturnOrderRequestVM
+            {
+                ReturnOrderRequest_ID = zz.Return_Order_Request_ID,
+                RequestOrderDate = zz.Request_Order_Date,
+                CustomerID = zz.Customer_ID,
+                Customer=zz.Customer,
+                OrderID=zz.Order_ID,
+                Order=zz.Order,
+
+            }).ToList();
+        }
+
+
+        // Get Return Order by ID
+
+        [System.Web.Http.Route("getReturnOrderByID/{id:int}")]
+        [System.Web.Mvc.HttpPost]
+        [HttpPost]
+        public object getReturnOrder(int id)
+
+        {
+
+            db.Configuration.ProxyCreationEnabled = false;
+
+            Return_Order_Request req = db.Return_Order_Request.Find(id);
+            if (req == null)
+            {
+                return NotFound();
+            }
+            return req;
+
+        }
+    }
+}
---
+++ OVS  API/ViewModels/Order/OrderVM.cs
@@ -0,0 +1,22 @@
+﻿using OVS_Team_3_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OVS_Team_3_API.ViewModels.Order
+{
+    public class OrderVM
+    {
+        public int Order_ID { get; set; }
+        public System.DateTime Order_Date { get; set; }
+        public System.DateTime Order_Finalizastion_Date { get; set; }
+        public bool Delivery { get; set; }
+        public Nullable<int> Customer_ID { get; set; }
+        public Nullable<int> Order_Status_ID { get; set; }
+        public Nullable<int> Employee_ID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
