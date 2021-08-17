636700a285f2ae5db920b14f3ac5af7c3e492287
pride1014 <83179646+pride1014@users.noreply.github.com>
2021-08-17 21:33:09 +0200

    Update

--- Team 3 Web Application/OrionWebApp/src/app/Employee/employees/employees.component.ts
+++ Team 3 Web Application/OrionWebApp/src/app/Employee/employees/employees.component.ts
@@ -14,48 +14,14 @@ import { OpenDialogComponent } from 'src/app/Dialog/open-dialog/open-dialog.comp
 })
 export class EmployeesComponent implements OnInit {
 
-  form: FormGroup = this.fb.group({
-    EmployeeName: ['', Validators.compose([Validators.required, Validators.maxLength(20), Validators.minLength(2)])],
-    EmployeeSurname: ['', Validators.compose([Validators.required, Validators.maxLength(20), Validators.minLength(2)])],
-    EmployeePhoneNumber: ['', Validators.compose([Validators.required, Validators.maxLength(20), Validators.minLength(2)])],
-    EmployeeEmailAddress: ['', Validators.compose([Validators.required, Validators.email])],
-  });
 
-  constructor(private service: ServicesService, private fb: FormBuilder, 
-    private snack: MatSnackBar, private dialogRef: MatDialogRef<EmployeesComponent>,
-    private router: Router) { }
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
 
-  RegisterEmployee() {
-    this.service.RegisterEmployee(this.form.value).subscribe(res => {
-      this.dialogRef.close();
-      this.snack.open('Successful registration', 'OK', {
-        horizontalPosition: 'center',
-        verticalPosition: 'bottom',
-        duration: 3000
-      });
-    }, (error: HttpErrorResponse) => {
-      if (error.status === 403) {
-        this.snack.open('This user has already been registered.', 'OK', {
-          horizontalPosition: 'center',
-          verticalPosition: 'bottom',
-          duration: 3000
-        });
-      }
-      this.snack.open('An error occurred on our servers, try again', 'OK', {
-        horizontalPosition: 'center',
-        verticalPosition: 'bottom',
-        duration: 3000
-      });
-      this.dialogRef.close();
-    })
-  }
 
-  back(){
-    this.router.navigateByUrl("Login")
-  }
 
 }
